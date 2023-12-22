6b887ed7e7f590f640dd2f4d6116e265e1861ec9
Neilyodoge <19980126why>
2023-12-22 17:21:11 +0800

    可以正确blit了，用CopyTexture的函数就行。试试把SNN挪进来

--- HDRP2022/Assets/1/3Kpost/Sharpen.cs
+++ HDRP2022/Assets/1/3Kpost/Sharpen.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Experimental.Rendering;
 
 [System.Serializable, VolumeComponentMenu("Post-processing/Kino/Sharpen")]
 public sealed class Sharpen : CustomPostProcessVolumeComponent, IPostProcessComponent
@@ -8,6 +9,7 @@ public sealed class Sharpen : CustomPostProcessVolumeComponent, IPostProcessComp
     public ClampedFloatParameter intensity = new ClampedFloatParameter(0, 0, 1);
 
     Material _material;
+    RTHandle temp;
 
     public bool IsActive() => _material != null && intensity.value > 0;
 
@@ -17,13 +19,23 @@ public sealed class Sharpen : CustomPostProcessVolumeComponent, IPostProcessComp
     public override void Setup()
     {
         _material = CoreUtils.CreateEngineMaterial("Hidden/Kino/PostProcess/Sharpen");
+
+        temp = RTHandles.Alloc(
+            Vector2.one, TextureXR.slices, dimension: TextureXR.dimension,
+            colorFormat: GraphicsFormat.B10G11R11_UFloatPack32, // We don't need alpha in the blur
+            useDynamicScale: true, name: "BlurBuffer");
     }
 
     public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle srcRT, RTHandle destRT)
     {
         if (_material == null) return;
+
+        //RenderTextureDescriptor opaquedesc = renderingData.cameraData.cameraTargetDescriptor;   // 拿到相机数据
+        //cmd.GetTemporaryRT(TempID1, opaquedesc); //申请一个临时图像
+
         _material.SetFloat("_Intensity", intensity.value);
-        _material.SetTexture("_InputTexture", srcRT);
+        cmd.CopyTexture(srcRT,temp);
+        _material.SetTexture("_InputTexture", temp);
         HDUtils.DrawFullScreen(cmd, _material, destRT);
     }
 
